8786ac5d379b5b7105f35e273a5aa7e08734ff90
Mohan Bandi <mohanbandi999@gmail.com>
2023-03-25 22:23:20 +0530

    sub

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { HttpService } from '../shared/http.service';
 
 @Component({
@@ -11,11 +12,31 @@ export class HttpComponent implements OnInit {
   constructor(private httpSer:HttpService) { }
 
   newdata:any;
-
+  users:any;
+  contacts$:any;
+  dataone:any;
+ status:any;
   ngOnInit(): void {
-  }
+  this.users=['ram','sita','radha','krishna'];
 
+  this.contacts$=of (this.users);
+  this.contacts$.subscribe((data: any)=>this.dataone=data)
+  
+
+  new Observable(obs=>{
+
+    obs.next("mohan");
+    obs.next("mohan 2")
+    obs.next("mohan 3")
+  }).subscribe(data=>{
+    this.status=data
+  });
+    
+  
+
+  
+  }
   getPosts(){
-    this.httpSer.getEmployee().subscribe(data=>this.newdata=data)
+    this.httpSer.getEmployee().subscribe(data=>this.newdata=data);
   }
 }
